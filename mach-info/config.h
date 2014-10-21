52d821f5fd2cf74e20426f1050b8da895891afa6
sancao2 <linjiangxiandong@163.com>
2014-10-21 23:29:15 +0800

    mirror from http://sf.net/projects/kmphpfm <3<3

---
+++ mach-info/config.h
@@ -0,0 +1,4 @@
+#define BUILT_TIME "2014-10-21 19:33:01"
+#define CC_VERSION "cc (SUSE Linux) 4.8.1 20130909 [gcc-4_8-branch revision 202388]"
+#define CXX_VERSION "c++ (SUSE Linux) 4.8.1 20130909 [gcc-4_8-branch revision 202388]"
+#define SYSTEM_TYPE "GNU/Linux i686"
